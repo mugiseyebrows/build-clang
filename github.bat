eedf3877d5849265563257a13c74b1c3c12e6ee0
Doronin Stanislav <mugisbrows@gmail.com>
2025-01-15 18:24:51 +0300

    fix

--- github.bat
+++ github.bat
@@ -1,7 +1,7 @@
 @echo off
 rem This file is generated from github.pbat, all edits will be lost
-set PATH=C:\Program Files\Git\cmd;C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build;C:\Miniconda3;C:\Miniconda3\Scripts;%USERPROFILE%\Miniconda3;%USERPROFILE%\Miniconda3\Scripts;C:\Program Files\CMake\bin;C:\Windows;C:\Windows\System32
-if not exist llvm-project git clone --depth 1 https://github.com/llvm/llvm-project.git
+set PATH=C:\Program Files\Git\cmd;C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build;C:\Windows;C:\Windows\System32
+if not exist llvm-project git clone https://github.com/llvm/llvm-project.git
 call vcvars64.bat
 where ninja > NUL 2>&1 || pip install ninja
 pushd llvm-project
@@ -11,6 +11,7 @@ pushd llvm-project
         ninja
     popd
 popd
+call vcvars64.bat
 pushd llvm-project\build
     ninja install
 popd
