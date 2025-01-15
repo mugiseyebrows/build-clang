33e1f33571b0fc8355424457a07b4c1ff9d9c751
Doronin Stanislav <mugisbrows@gmail.com>
2025-01-15 12:12:29 +0300

    fix

--- github.bat
+++ github.bat
@@ -1,9 +1,9 @@
 @echo off
 rem This file is generated from github.pbat, all edits will be lost
-set PATH=C:\Program Files\Git\cmd;C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build;%PATH%
+set PATH=C:\Program Files\Git\cmd;C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build;C:\Miniconda3;C:\Miniconda3\Scripts;%USERPROFILE%\Miniconda3;%USERPROFILE%\Miniconda3\Scripts;C:\Program Files\CMake\bin;C:\Windows;C:\Windows\System32
 if not exist llvm-project git clone --depth 1 https://github.com/llvm/llvm-project.git
-dir C:\llvm18 || echo ok
 call vcvars64.bat
+where ninja > NUL 2>&1 || pip install ninja
 pushd llvm-project
 if not exist build mkdir build
 pushd build
